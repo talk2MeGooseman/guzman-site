15dbc95fcfc27b8d373a1df0d596be30afa8b01a
Erik Guzman <erik.guzman@guzman.codes>
2020-10-26 20:45:36 -0700

    Simple nes static site

--- src/App.js
+++ src/App.js
@@ -4,20 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class="nes-container left">
+        <p>Erik Guzman Codes</p>
       </header>
+      <section class="nes-container is-dark chat-container ">
+        <section class="message-list">
+          <section class="message -right">
+            <div class="nes-balloon from-right is-dark">
+              <p>Hey stranger my name in Erik Guzman and welcome to my comfy little corner of the the internet.</p>
+            </div>
+            <div className="right">
+              <img className="nes-avatar is-large" alt="Guzman Avatar" src="https://imgur.com/gVPbr9s.jpg" style={ {'image-rendering': 'pixelated'} }/>
+            </div>
+          </section>
+          <section class="message -left">
+            <div class="nes-balloon from-left is-dark">
+              <p>Here you will find a little information about me and what I do.</p>
+            </div>
+            <div className="left">
+              <img className="nes-avatar is-large" alt="Guzman Avatar" src="https://imgur.com/gVPbr9s.jpg" style={ {'image-rendering': 'pixelated'} }/>
+            </div>
+          </section>
+        </section>
+      </section>
+      <h1>To be continued...</h1>
     </div>
   );
 }
